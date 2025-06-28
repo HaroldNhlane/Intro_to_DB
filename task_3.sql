3a1d2633bd6c804d4f3e29e46827cdec822f8dfc
Harold <haroldkaluba8@gmail.com>
2025-06-28 18:26:29 +0300

    Add alx_book_store

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,5 @@
 -- task_3.sql
-
+USE alx_book_store; -- Make sure to use the correct database name (case-insensitive in many setups, but consistent is best)
 -- List all tables in the specified database.
 -- The database name will be passed as an argument to the MySQL command.
 SHOW TABLES;
