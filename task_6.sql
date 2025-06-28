6ce83e372e2b8592fff7f5103e92cff301c4ba17
Harold <haroldkaluba8@gmail.com>
2025-06-28 20:20:19 +0300

    Add task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,7 +3,7 @@
 -- Inserts multiple rows into the 'customers' table in the 'alx_book_store' database.
 -- The database name will be passed as an argument to the MySQL command.
 
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
+INSERT INTO customers (customer_id, customer_name, emailL, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
