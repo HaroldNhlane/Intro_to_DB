2119e8064c4f84b42451d09139a0db5aa5a77d53
Harold <haroldkaluba8@gmail.com>
2025-06-28 18:50:16 +0300

    Modified task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,20 +1,12 @@
 -- task_4.sql
+-- This script shows the full description of the books table without using DESCRIBE or EXPLAIN
 
--- Prints the full description of the 'books' table
--- from the database 'alx_book_store' using INFORMATION_SCHEMA.
--- The database name will be passed as an argument to the mysql command.
--- DESCRIBE and EXPLAIN statements are not allowed.
-
-SELECT
-    COLUMN_NAME AS FIELD,
-    COLUMN_TYPE AS TYPE,
-    IS_NULLABLE AS 'NULL',
-    COLUMN_KEY AS KEY,
-    COLUMN_DEFAULT AS DEFAULT,
-    EXTRA
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'ALX_BOOK_STORE' AND TABLE_NAME = 'BOOKS'
-ORDER BY
-    ORDINAL_POSITION;
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
