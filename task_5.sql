7a601ef373377afa1179d6dbe413f6b14e45ed10
Harold <haroldkaluba8@gmail.com>
2025-06-28 18:59:29 +0300

    Add task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- task_5.sql
+
+-- Inserts a single row into the 'customers' table in the 'alx_book_store' database.
+-- The database name will be passed as an argument to the MySQL command.
+
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
+(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
