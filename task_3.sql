0b141139eb9f7932f73cdc7bbc64ba2bec69c7d5
Harold <haroldkaluba8@gmail.com>
2025-06-28 19:57:31 +0300

    changed to lower case task_5

--- task_3.sql
+++ task_3.sql
@@ -2,4 +2,3 @@
 USE alx_book_store; -- Make sure to use the correct database name (case-insensitive in many setups, but consistent is best)
 -- List all tables in the specified database.
 -- The database name will be passed as an argument to the MySQL command.
-SHOW TABLES;
--- task_5.sql
+++ task_5.sql
@@ -3,5 +3,5 @@
 -- Inserts a single row into the 'customers' table in the 'alx_book_store' database.
 -- The database name will be passed as an argument to the MySQL command.
 
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
